e12dbb90843721501e696141e3cd9834ac0c3707
Your Name <you@example.com>
2023-11-30 14:57:46 +0000

    updated responsivness

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
+const desktopBg = document.querySelector(".desktop_bg");
+const mobileBg = document.querySelector(".mobile_bg");
+
+
 
 
 // event listener
@@ -11,3 +15,19 @@ hamburger.addEventListener("click", () => {
     navbar.classList.toggle("active");
 })
 
+
+// toggle hero image based in screen size
+
+window.addEventListener('resize', () => {
+
+    let width = window.innerWidth;
+    console.log(width)
+
+    if (width <= 1051){
+        desktopBg.classList.add("hide_bg");
+        mobileBg.classList.remove("hide_bg");
+    } else{
+        desktopBg.classList.remove("hide_bg");
+        mobileBg.classList.add("hide_bg");
+    }
+})
