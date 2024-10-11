4a40291e0215626076c0f2443df73845cdc44b22
Your Name <you@example.com>
2024-10-11 19:24:55 +0100

    updates vantajs

---
+++ birds.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.NET({
+    el: "#home_birds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xffffff,
+    backgroundColor: 0x70707,
+    points: 8.0,
+    maxDistance: 17.0,
+    spacing: 20.0,
+  });
+});
