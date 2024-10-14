f288fea7ead5ad1922bd6731846985b00c83263b
Your Name <you@example.com>
2024-10-14 16:14:24 +0100

    hamburger menu

---
+++ script.js
@@ -0,0 +1,20 @@
+// mobile menu
+
+// dom elements
+let burger = document.querySelector(".hamburger");
+let icon = document.querySelector(".icon");
+
+let mobileMenu = document.querySelector(".mobileNav");
+
+// when burger menu clicked toggle nav
+burger.addEventListener("click", function () {
+  mobileMenu.classList.toggle("showNav");
+
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+});
